e9291dd067f0ca79a6dc804518a13e6f037e30ed
anand <anand.ram@nile-global.com>
2021-05-13 07:53:37 +0530

    Binary seah implementation.

---
+++ SearchAlgos/binary_search.py
@@ -0,0 +1,19 @@
+def binary_search(input_list,ele):
+    min = 0
+    max = len(input_list)
+    mid = 0
+
+    while min < max:
+        mid = (min+max)//2
+        if input_list[mid] > ele:
+            min = mid + 1
+        elif input_list[mid] < ele:
+            max = mid -1
+        else:
+            return mid
+        return -1
+
+
+
+val = binary_search([1,2,3,4,5], 3)
+print(val)
